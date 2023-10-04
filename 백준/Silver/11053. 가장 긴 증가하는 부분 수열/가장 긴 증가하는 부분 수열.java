f7e718f18d4473caa7266dd0bb72056f479e6936
ekfkawl <99597985+ekfkawl@users.noreply.github.com>
2023-10-04 20:54:34 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 148 ms, Memory: 14652 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static int N;
+    static int[] arr;
+    static int[] dp = new int[1001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+
+//        10
+//        10 20 10 30 20 10 10 20 50 10 80
+
+        int max = 1;
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && dp[i] <= dp[j]) {
+                    dp[i] = dp[j] + 1;
+                }
+                max = Math.max(max, dp[i]);
+            }
+        }
+
+        System.out.println(max);
+    }
+
+
+}
