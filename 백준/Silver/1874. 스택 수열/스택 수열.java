6623a1131a4b6ca92799b2a904b3a4b3b96e693a
ekfkawl <99597985+ekfkawl@users.noreply.github.com>
2024-01-22 17:29:50 +0900

    [Silver II] Title: 스택 수열, Time: 360 ms, Memory: 27324 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,42 +1,38 @@
 
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
 
-        try (
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        ) {
-            int N = Integer.parseInt(br.readLine()); // 명령어 개수만큼 반복하기 위함
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int position = 0;
 
-            int position = 0;
-            int halt = 0;
-
-            for (int i = 1; i <= N; i++) {
-                int v = Integer.parseInt(br.readLine());
-                if (position < v) {
-                    for (int j = position; j < v; j++) {
-                        sb.append("+\n");
-                        position++;
-                        stack.push(position);
-                    }
+        for (int i = 1; i <= N; i++) {
+            int v = Integer.parseInt(br.readLine());
+            if (position < v) {
+                for (int j = position; j < v; j++) {
+                    sb.append("+\n");
+                    position++;
+                    stack.push(position);
                 }
-                if (stack.peek() != v) {
-                    sb.setLength(0);
-                    sb.append("NO\n");
-                    break;
-                }
-                sb.append("-\n");
-                stack.pop();
             }
-            bw.write(sb.toString());
-            bw.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
+            if (stack.peek() != v) {
+                sb.setLength(0);
+                sb.append("NO\n");
+                break;
+            }
+            sb.append("-\n");
+            stack.pop();
         }
+        
+        System.out.println(sb.toString());
     }
 }
