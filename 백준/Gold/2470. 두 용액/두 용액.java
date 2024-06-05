7206e7de2bae88c2dc659655e0c01b44f5dde4cb
ekfkawl <99597985+ekfkawl@users.noreply.github.com>
2024-06-05 17:39:09 +0900

    [Gold V] Title: 두 용액, Time: 412 ms, Memory: 31012 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,41 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static int N;
+    static int[] nums;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+
+        int l = 0;
+        int r = N-1;
+        int min = Integer.MAX_VALUE;
+        int lv = 0;
+        int rv = 0;
+
+        while (l < r) {
+            int sum = nums[l] + nums[r];
+            if (min > Math.abs(sum)) {
+                min = Math.abs(sum);
+
+                lv = nums[l];
+                rv = nums[r];
+            }
+
+            if (sum > 0) {
+                r--;
+            } else {
+                l++;
+            }
+        }
+
+        System.out.print(lv + " " + rv);
+    }
+}
