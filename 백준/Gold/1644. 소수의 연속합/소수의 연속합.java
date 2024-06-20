de01cff710ed0460f29d4d47fc56091895bda32a
ekfkawl <99597985+ekfkawl@users.noreply.github.com>
2024-06-20 15:29:25 +0900

    [Gold III] Title: 소수의 연속합, Time: 772 ms, Memory: 23208 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -0,0 +1,84 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        if (N == 2) {
+            System.out.println(1);
+            return;
+        }
+
+        List<Integer> primes = new ArrayList<>();
+        primes.add(2);
+
+        int n = 3;
+        while (n <= N) {
+            if (isPrime(n)) {
+                primes.add(n);
+            }
+
+            n += 2;
+        }
+
+        int l = 0;
+        int r = 0;
+
+        int cur = 0;
+        int count = 0;
+
+        while (r <= primes.size()) {
+            if (cur < N) {
+                if (r == primes.size()) {
+                    break;
+                }
+                cur += primes.get(r);
+                r++;
+            }else {
+                cur -= primes.get(l);
+                l++;
+            }
+
+            if (cur == N) {
+                count++;
+            }
+        }
+
+
+        System.out.println(count);
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+
+        if (n == 2) {
+            return true;
+        }
+
+        if (n % 2 == 0) {
+            return false;
+        }
+
+        int sqrt = (int) Math.sqrt(n);
+        for (int i = 3; i <= sqrt; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
