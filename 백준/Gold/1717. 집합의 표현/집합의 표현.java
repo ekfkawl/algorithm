715fce5e0ea0c69bbe6ae83429620e648f25c536
ekfkawl <99597985+ekfkawl@users.noreply.github.com>
2024-08-27 16:21:18 +0900

    [Gold V] Title: 집합의 표현, Time: 1200 ms, Memory: 87412 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static int N, M;
+    static int[] parent, rank;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
+
+        parent = new int[N + 1];
+        rank = new int[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            int[] v = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int command = v[0];
+            int a = v[1];
+            int b = v[2];
+
+            if (command == 0) {
+                union(a, b);
+            }else if (find(a) == find(b)) {
+                System.out.println("YES");
+            }else {
+                System.out.println("NO");
+            }
+        }
+
+    }
+
+    static int find (int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    static void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+}
