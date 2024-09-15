e52607c7fe2641e9abedc65f473c846d8a443fcf
ekfkawl <99597985+ekfkawl@users.noreply.github.com>
2024-09-16 03:31:43 +0900

    [level 2] Title: 전화번호 목록, Time: 560.14 ms, Memory: 339 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,90 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    private static class TrieNode {
+        TrieNode[] child = new TrieNode[10];
+        String key;
+        boolean eof;
+    }
+
+    public static class Trie {
+        private TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        public void insert(String key) {
+            TrieNode node = this.root;
+
+            for (int i = 0; i < key.length(); i++) {
+                int idx = key.charAt(i) - '0';
+
+                if (node.child[idx] == null) {
+                    node.child[idx] = new TrieNode();
+                }else {
+                    node.child[idx].eof = false;
+                }
+
+                node = node.child[idx];
+            }
+
+            node.key = key;
+            node.eof = true;
+        }
+
+        public List<String> findByPrefix(String key) {
+            List<String> res = new ArrayList<>();
+
+            TrieNode node = this.root;
+            for (int i = 0; i < key.length(); i++) {
+                int idx = key.charAt(i) - '0';
+
+                if (node.child[idx] == null) {
+                    return res;
+                }
+
+                node = node.child[idx];
+            }
+
+            findByPrefix(node, res);
+
+            return res;
+        }
+
+        private void findByPrefix(TrieNode node, List<String> res) {
+            if (node == null) {
+                return;
+            }
+
+            if (node.eof || node.key != null) {
+                res.add(node.key);
+            }
+
+            for (TrieNode child : node.child) {
+                findByPrefix(child, res);
+            }
+        }
+    }
+
+
+    public static boolean solution(String[] phone_book) {
+        Trie trie = new Trie();
+
+        for (String s : phone_book) {
+            trie.insert(s);
+        }
+
+        for (String s : phone_book) {
+            List<String> prefix = trie.findByPrefix(s);
+            if (prefix.size() > 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
