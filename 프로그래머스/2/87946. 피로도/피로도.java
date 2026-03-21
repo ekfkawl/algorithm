02205764ee06bd337257d10540719ec5094a180f
ekfkawl <99597985+ekfkawl@users.noreply.github.com>
2026-03-22 02:13:56 +0900

    [level 2] Title: 피로도, Time: 3.15 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,29 +1,28 @@
 class Solution {
     static boolean[] visited;
-    static int maxCount = 0;
     
-    static void dfs(int depth, int k, int[][] dungeons) {
-        maxCount = Math.max(maxCount, depth);
+    static int dfs(int depth, int k, int[][] dungeons) {
+        int res = depth;
 
         if (depth == dungeons.length) {
-            return;
+            return res;
         }
 
         for (int i = 0; i < dungeons.length; i++) {
             if (!visited[i]) {
                 visited[i] = true;
                 if (k >= dungeons[i][0]) {
-                    dfs(depth + 1, k - dungeons[i][1], dungeons);
+                    res = Math.max(res, dfs(depth + 1, k - dungeons[i][1], dungeons));
                 }
                 visited[i] = false;
             }
         }
-
+        
+        return res;
     }
 
     public static int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
-        dfs(0, k, dungeons);
-        return maxCount;
+        return dfs(0, k, dungeons);
     }
 }
